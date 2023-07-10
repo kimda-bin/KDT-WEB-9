84dd25426df640d729eff09dd91b3688a817d62e
kimda-bin <rlaekqls0504@gmail.com>
2023-07-10 16:51:37 +0900

    study: 파일 경로 변경, javascript 개념

---
+++ javascript/index.js
@@ -0,0 +1,23 @@
+// 변수란 특정 값을 저장하는 공간
+// abc = "abcdef~~"
+
+// 키워드 변수이름 = 값
+// 키워드 : var, let, const
+// var 보단 let을 권장
+
+var number = 5; // 변수의 선언과 할당이 동시에
+
+var number1; // 변수 선언
+number1 = 6; // 변수에 값을 할당
+
+
+let string = "가나다라";
+
+// var let const
+// var : 재선언, 재할당 가능
+// let : 재선언 불가, 재할당 가능
+// const : 재선언 불가, 재할당 불가, 무조건 변수 선언, 할당 동시에 해야한다
+
+let lll;
+console.log(lll);
+
