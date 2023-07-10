726a7c17617ed85937fa119414827caefaaa8785
kimda-bin <rlaekqls0504@gmail.com>
2023-07-10 20:01:14 +0900

    study: js 실습2,3,4

---
+++ javascript/js_T2+T3+T4.js
@@ -0,0 +1,23 @@
+        // object 실습
+        // let i = {
+        //     name: "김다빈",
+        //     mbti: "ISFP",
+        //     home: "인천",
+        // }
+
+        // console.log("안녕하세요! 제 이름은 ", i.name + " 입니다");
+        // console.log("제 MBTI는 ", i.mbti, " 이고");
+        // console.log("사는곳은 ", i.home, "입니다");
+
+
+         // typeof 실습
+        // console.log(`${typeof (5)} isn't ${typeof ("문자열")} data type.`)
+        // console.log(`Typeof를 ${typeof (true)}이나 null에 사용하면, ${typeof (false)} 결과를 얻을 수 있습니다.`)
+
+
+        // 형변환 실습
+        // let mathScore = "77";
+        // let engScore = "88";
+
+        // let avgScore = (Number(mathScore) + Number(engScore)) / 2;
+        // console.log(avgScore)
