36ff69ff515b4ac4bb0f3caded62fff332d31ba0
kimda-bin <rlaekqls0504@gmail.com>
2023-07-27 20:50:12 +0900

    node.js 공부

--- 07_nodejs/index.js
+++ 07_nodejs/index.js
@@ -1,3 +1,31 @@
 // 키값을 불러오는 것
-const { b } = require("./module");
-console.log(b)
+// const { b } = require("./module");
+// console.log(b)
+
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer(function (req, res) {
+    // // 요청하고 나서 번호
+    // res.writeHead(200)
+
+    // // 응답 본문 작성
+    // res.write("<h1>hello world</h1>")
+
+    // // 응답 본문 작성 후 응답 종료
+    // res.end("<p>end</p>")
+
+    // 파일전송
+    try {
+        const data = fs.readFileSync('./index.html')
+        res.writeHead(200)
+        res.end(data)
+    } catch (error) {
+        console.log(error)
+        res.writeHead(404)
+        res.end(error.message)
+    }
+});
+
+server.listen(8000, function () {
+    console.log('8000번 포트로 실행')
+})
