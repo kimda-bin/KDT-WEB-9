8595c28deef95ac64dab55b41de3252c9f72e2cc
kimda-bin <rlaekqls0504@gmail.com>
2023-08-02 19:54:32 +0900

    study: 파일이동 + 동적 form 연습 + 실습

---
+++ 11_form_dynamic/index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+// body-parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// views engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// router
+app.get('/', (req, res) => {
+    res.render('index')
+});
+// --------------------------------------------------------------------
+// ajax
+// 2. index.ejs의 ajax에서 보낸 데이터가 req.query에 담기고
+app.get('/ajax', (req, res) => {
+    console.log('back', req.query)
+    // 3. req.query값을 다시 ajax로 보낸다
+    res.send(req.query)
+})
+
+app.post('/ajax', (req, res) => {
+    console.log('back', req.body)
+    res.send(req.body)
+})
+
+
+// axios
+// 2.  index.ejs의 axios에서 보낸 데이터가 req.query에 담기고
+app.get('/axios', (req, res) => {
+    console.log('back', req.query)
+    // 3. req.query값을 다시 axios로 보낸다
+    res.send(req.query)
+})
+
+app.post('/axios', (req, res) => {
+    console.log('back', req.body)
+    res.send(req.body)
+})
+
+
+// fetch
+app.get('/fetch', (req, res) => {
+    console.log('back', req.query)
+    res.send(req.query)
+})
+
+app.post('/fetch', (req, res) => {
+    console.log('back', req.body)
+    res.send(req.body)
+})
+
+// --------------------------------------------------------------------
+
+// T1
+app.get('/T1', (req, res) => {
+    res.render('T1')
+});
+
+app.get('/register_result', (req, res) => {
+    console.log('back', req.query)
+    res.send(req.query)
+})
+
+// T2
+app.get('/T2', (req, res) => {
+    res.render('T2')
+});
+
+app.post('/login_result', (req, res) => {
+    console.log('back', req.body)
+    const id = 1234
+    const pw = 1234
+    if (req.body.id == id && req.body.pw == pw) {
+        res.send("true")
+    } else {
+        res.send("false")
+    }
+})
+
+
+// --------------------------------------------------------------------
+
+// server start
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+});
